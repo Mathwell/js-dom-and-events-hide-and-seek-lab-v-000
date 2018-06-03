c90becd3a63fb3438ae4692079600a28f4e72861
Olena Ageyeva <eageeva@hotmail.com>
2018-06-03 13:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n){
  let ranks=document.querySelectorAll('.ranked-list')
  ranks.forEach(rank=>{
    let children=rank.children
-   for(i=0; i<children.length; i++){
+   for(let i=0; i<children.length; i++){
      children[i].innerHTML=parseInt(children[i].innerHTML)+n
    }
  })
