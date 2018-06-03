10588d110aaaa0bf66ba4d54db6c0583184a4bb5
Olena Ageyeva <eageeva@hotmail.com>
2018-06-03 13:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function nestedTarget(){
 
 function increaseRankBy(n){
  let ranks=document.querySelectorAll('.ranked-list')
- ranks.forEach()
+ ranks.forEach(rank=>{
+   children=rank.children
+   return children.forEach(child =>{
+     child.innerHTML=parseInt(child.innerHTML)+n
+   })
+ })
 }
 
 function deepestChild(){
